f7cc791f994d444bba29d4a8d7ccd26dc257cfc7
basmala-hazem <164567169+basmala-hazem@users.noreply.github.com>
2026-04-19 10:47:30 +0200

    Add files via upload
    try

---
+++ Form1.Designer.cs
@@ -0,0 +1,357 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class DashForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridViewDash = new System.Windows.Forms.DataGridView();
+            this.members_btn = new System.Windows.Forms.Button();
+            this.Names = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.planCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.expiryCol = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDash)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridViewDash
+            // 
+            this.dataGridViewDash.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridViewDash.ColumnHeadersHeight = 34;
+            this.dataGridViewDash.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Names,
+            this.planCol,
+            this.expiryCol,
+            this.Column1});
+            this.dataGridViewDash.Location = new System.Drawing.Point(12, 278);
+            this.dataGridViewDash.Name = "dataGridViewDash";
+            this.dataGridViewDash.RowHeadersWidth = 62;
+            this.dataGridViewDash.RowTemplate.Height = 35;
+            this.dataGridViewDash.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridViewDash.Size = new System.Drawing.Size(718, 377);
+            this.dataGridViewDash.TabIndex = 1;
+            this.dataGridViewDash.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridViewDash_CellContentClick);
+            // 
+            // members_btn
+            // 
+            this.members_btn.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.members_btn.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.members_btn.Location = new System.Drawing.Point(12, 192);
+            this.members_btn.Name = "members_btn";
+            this.members_btn.Size = new System.Drawing.Size(198, 64);
+            this.members_btn.TabIndex = 2;
+            this.members_btn.Text = "Add Member";
+            this.members_btn.UseVisualStyleBackColor = false;
+            this.members_btn.Click += new System.EventHandler(this.members_btn_Click);
+            // 
+            // Names
+            // 
+            this.Names.HeaderText = "Names";
+            this.Names.MinimumWidth = 8;
+            this.Names.Name = "Names";
+            this.Names.Width = 200;
+            // 
+            // planCol
+            // 
+            this.planCol.HeaderText = "Plan";
+            this.planCol.MinimumWidth = 8;
+            this.planCol.Name = "planCol";
+            this.planCol.Width = 150;
+            // 
+            // expiryCol
+            // 
+            this.expiryCol.HeaderText = "Expiry Date";
+            this.expiryCol.MinimumWidth = 8;
+            this.expiryCol.Name = "expiryCol";
+            this.expiryCol.Width = 150;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Status";
+            this.Column1.MinimumWidth = 8;
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 150;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(499, 192);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(198, 64);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Finance";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(993, 192);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(198, 64);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Gym Store";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(749, 192);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(198, 64);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Member Details";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(260, 192);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(198, 64);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Coaches";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(387, 116);
+            this.panel1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(108, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(223, 39);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Total Members ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(160, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(96, 39);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "label2";
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel2.Location = new System.Drawing.Point(417, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(374, 116);
+            this.panel2.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(162, 61);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(96, 39);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(112, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 39);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Total Trainers ";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.CadetBlue;
+            this.panel3.Controls.Add(this.pictureBox3);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Font = new System.Drawing.Font("Comic Sans MS", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel3.Location = new System.Drawing.Point(809, 12);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(394, 116);
+            this.panel3.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(177, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(96, 39);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "label5";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(76, 9);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(288, 39);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Active Subscriptions";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::WindowsFormsApp1.Properties.Resources.brett_jordan_U2q73PfHFpM_unsplash;
+            this.pictureBox4.Location = new System.Drawing.Point(827, 278);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(364, 377);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 10;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::WindowsFormsApp1.Properties.Resources.calendar;
+            this.pictureBox3.Location = new System.Drawing.Point(-12, 51);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::WindowsFormsApp1.Properties.Resources.trainer;
+            this.pictureBox2.Location = new System.Drawing.Point(-12, 50);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 10;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.users_group;
+            this.pictureBox1.Location = new System.Drawing.Point(-11, 51);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 10;
+            this.pictureBox1.TabStop = false;
+            // 
+            // DashForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(1215, 667);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.members_btn);
+            this.Controls.Add(this.dataGridViewDash);
+            this.Name = "DashForm";
+            this.Text = "Dashboard";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewDash)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        public System.Windows.Forms.DataGridView dataGridViewDash;
+        private System.Windows.Forms.Button members_btn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Names;
+        private System.Windows.Forms.DataGridViewTextBoxColumn planCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn expiryCol;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
